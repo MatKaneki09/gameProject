70c588edd8e68dc162566ae2546a5e91a55e17d9
MatKaneki09 <96303655+MatKaneki09@users.noreply.github.com>
2025-02-25 13:35:55 +0100

    Create models.py
    ajout d'un dossier fonction
    modification de l'app.py
    segmentation en plusieurs fichier .py

---
+++ fonction/models.py
@@ -0,0 +1,25 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    iduser = db.Column(db.Integer, primary_key=True)
+    pseudo = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
+    collection = db.relationship('Collection', backref='owner', lazy=True)
+
+class Collection(db.Model):
+    idcollection = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.iduser'), nullable=False)
+    cartes = db.relationship('Carte_Collection', backref='collection', lazy=True)
+
+class Carte(db.Model):
+    idcarte = db.Column(db.Integer, primary_key=True)
+    nom = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.String(200), nullable=True)
+
+class Carte_Collection(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    idcollection = db.Column(db.Integer, db.ForeignKey('collection.idcollection'), nullable=False)
+    idcarte = db.Column(db.Integer, db.ForeignKey('carte.idcarte'), nullable=False)
+    quantite = db.Column(db.Integer, nullable=False)
